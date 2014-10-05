21322a54c56fd3bdb252518c007ed6b75707556a
Rathesh Saba <rathesh.saba@gmail.com>
2014-10-05 15:21:18 -0400

    Text to Speech implemented

---
+++ app/src/main/java/com/example/rathsaba/shoutnotifications/MyActivity.java
@@ -0,0 +1,60 @@
+package com.example.rathsaba.shoutnotifications;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        final Button button = (Button) findViewById(R.id.button);
+        final EditText editText   = (EditText)findViewById(R.id.editText);
+        final TextToSpeech tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    //Speak
+                }
+                else {
+                    //Handle initialization error here
+                }
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String x = editText.getText().toString();
+                tts.speak(x, TextToSpeech.QUEUE_ADD, null);
+                int i = 3;
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
